1025d42ce0706f36c416e60a012ecf32b8548c57
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-11-06 12:55:23 +0100

    Add a --start option to csv-add-id

--- csv-add-id.hs
+++ csv-add-id.hs
@@ -1,5 +1,6 @@
 import System.Environment
 import Database.TxtSushi.FlatFile
+import Data.Char
 
 type Tbl = [[String]]
 
@@ -17,13 +18,18 @@ addIdLn :: Int -> [String] -> [String]
 addIdLn 0 row = "ID":row
 addIdLn i row = show i:row
 
-addId :: FilePath -> IO ()
-addId = interactTable (zipWith addIdLn [0..])
+addId :: Int -> FilePath -> IO ()
+addId i = interactTable (zipWith addIdLn [i..])
 
 main :: IO ()
 main = do
     args <- getArgs
-    case args of
-      [fileArg]  -> addId fileArg
-      []         -> addId "-"
-      _          -> error "Usage: csv-add-id [<file-or-dash>]"
+    let (args2, start) =
+          case args of
+            "--start":i:xs
+              | all isDigit i -> (xs, read i)
+            _                 -> (args, 0)
+    case args2 of
+      [fileArg]  -> addId start fileArg
+      []         -> addId start "-"
+      _          -> error "Usage: csv-add-id [--start <int>] [<file>|-]"
